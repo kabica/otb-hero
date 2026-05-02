5019f4debaa7d65964e4d8be55eade4f4b6da2f9
Kevin Alexander Bica <alex@Kevins-MacBook-Pro.local>
2026-05-02 13:38:03 -0600

    new eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,23 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
 ])
